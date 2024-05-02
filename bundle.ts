fbf1414992b2bfe820d4f42736d18b71784f94a8
5C6F2F <5c6f2f@gmail.com>
2024-05-03 00:07:12 +0900

    [update] #5 bundle時の動作を変更

--- bundle.ts
+++ bundle.ts
@@ -1,5 +1,14 @@
 import { bundle } from "https://deno.land/x/emit@0.38.2/mod.ts";
 
-const url = new URL(import.meta.resolve("./src/main.ts"));
-const { code } = await bundle(url);
-Deno.writeTextFile("./dist/main.js", code);
+{
+  const url = new URL(import.meta.resolve("./src/content/main.ts"));
+  const { code } = await bundle(url);
+  await Deno.mkdir("./dist/content", { recursive: true });
+  await Deno.writeTextFile("./dist/content/main.js", code);
+}
+{
+  const url = new URL(import.meta.resolve("./src/popup/main.ts"));
+  const { code } = await bundle(url);
+  await Deno.mkdir("./dist/popup", { recursive: true });
+  await Deno.writeTextFile("./dist/popup/main.js", code);
+}
