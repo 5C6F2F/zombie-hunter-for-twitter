0277bb6e5515d43d6ae97a940ead5d275c0a7282
5C6F2F <5c6f2f@gmail.com>
2024-05-07 20:05:43 +0900

    [update] #8 一部変数・関数等の名前を変更

--- src/lib/zombie.ts
+++ src/lib/zombie.ts
@@ -83,9 +83,9 @@ export class Zombie extends User {
             </g>
           </svg>
         </div>
-        <div class="block-user">
+        <div class="purge-zombie">
           <!-- アイコンは https://icon-pit.com/pictogram/1661 「禁止マークのアイコン」を使用させていただきました。 -->
-          <img src="../icons/blockButton.png" alt="" class="block-user-button">
+          <img src="../icons/purgeButton.png" alt="" class="purge-zombie-button">
         </div>
       </div>`;
 
