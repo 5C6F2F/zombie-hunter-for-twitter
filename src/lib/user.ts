80f86e5cf3661ecc2e806d233563995c2e246bb5
5C6F2F <5c6f2f@gmail.com>
2024-08-19 17:25:11 +0900

    [update] main フォーマット

--- src/lib/user.ts
+++ src/lib/user.ts
@@ -57,13 +57,21 @@ export function userFromTweet(tweet: Element): User | null {
 
 export function getUserInfo(
   tweet: Element
-): [string | null | undefined, string | null | undefined, string | null | undefined] {
+): [
+  string | null | undefined,
+  string | null | undefined,
+  string | null | undefined
+] {
   const name_id_element = tweet.querySelector(userNameIdSelector);
 
   // name_id_element?.children[0]?.textContent とするとタイムライン中ではツイート日時ごと取得してしまう。
   // しかし良い感じのセレクターもないためこんなことに。
-  const name = name_id_element?.children[0].firstChild?.firstChild?.firstChild?.firstChild?.firstChild?.textContent;
-  const id = name_id_element?.children[1].firstChild?.firstChild?.firstChild?.firstChild?.firstChild?.textContent;
+  const name =
+    name_id_element?.children[0].firstChild?.firstChild?.firstChild?.firstChild
+      ?.firstChild?.textContent;
+  const id =
+    name_id_element?.children[1].firstChild?.firstChild?.firstChild?.firstChild
+      ?.firstChild?.textContent;
 
   const time_elements = tweet.getElementsByTagName("time");
   // 引用リツイートの際に引用元の要素を取得しないよう、最後の要素を取得する。
