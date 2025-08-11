8b5c8244dcf26b3b96e95158308a3542bde1f28a
5C6F2F <5c6f2f@gmail.com>
2025-08-12 01:50:36 +0900

    [add] #29 定数を追加

--- src/popup/consts.ts
+++ src/popup/consts.ts
@@ -1,8 +1,16 @@
+// ----- HTML -----
+
+// 通報開始ボタンと累計通報数のラッパー
+export const allPurgeAndPurgeCounts = "all-purge-and-purge-counts";
+
 // 処理開始ボタン
 export const allPurgeButtonId = "all-purge-button";
 
 // 処理を止めるボタン
 export const allPurgeStopButtonId = "all-purge-stop-button";
+
+// 累計通報数
+export const totalPurgeCountsId = "total-purge-counts-span";
 
 // ゾンビの数
 export const zombiesNumId = "zombies-num";
@@ -31,3 +39,8 @@ export const removeUserClassName = "remove-user";
 
 // 対象を処理するボタン
 export const purgeZombieClassName = "purge-zombie";
+
+// ----- TypeScript -----
+
+// 累計通報数をストレージに保存する際のキー
+export const totalPurgeCountsKeyForStorage = "totalPurgeCountsKey";
