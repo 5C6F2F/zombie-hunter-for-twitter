e797ba1d7634ae10917182f77ac7474574b1c64c
5C6F2F <5c6f2f@gmail.com>
2025-08-18 18:57:20 +0900

    [add] 設定の読み込み機能を追加

---
+++ src/lib/settings.ts
@@ -0,0 +1,42 @@
+import { colorModeSettingKeyForStorage } from "./consts.ts";
+
+export const enum ColorMode {
+  Light,
+  DarkBlueAndBlack,
+}
+
+export class Settings {
+  private colorMode: ColorMode;
+
+  constructor() {
+    this.colorMode = ColorMode.Light;
+  }
+
+  async loadSettingsFromStorage(): Promise<this> {
+    await new Promise<void>((resolve) => {
+      chrome.storage.local.get(colorModeSettingKeyForStorage, (settings) => {
+        this.colorMode = this.parseColorModeFromStorage(
+          settings[colorModeSettingKeyForStorage],
+        );
+        resolve();
+      });
+    });
+
+    return this;
+  }
+
+  private parseColorModeFromStorage(value: string): ColorMode {
+    if (value == "light") {
+      return ColorMode.Light;
+    } else if (value == "dark-blue-and-black") {
+      return ColorMode.DarkBlueAndBlack;
+    } else {
+      // 未設定の場合、デフォルトはライトモード
+      return ColorMode.Light;
+    }
+  }
+
+  get getColorMode(): ColorMode {
+    return this.colorMode;
+  }
+}
